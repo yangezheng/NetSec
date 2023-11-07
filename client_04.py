95c82cb192db1c45dead3c11735ec458d708f79f
yzheng <posterscofield@gmail.com>
2023-11-07 17:28:46 +0100

    did exercise4

---
+++ client_04.py
@@ -0,0 +1,31 @@
+## Challenge 01
+#!/usr/bin/env python3
+import socket
+# Fill in the right target here
+HOST = 'netsec.net.in.tum.de'    # TODO
+PORT = 56915                     # TODO
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((HOST, PORT))
+sf = s.makefile("rw")  # we use a file abstraction for the sockets
+
+def read(sf):
+    data = sf.readline().rstrip('\n')
+    print("From Server: `{}'".format(data))
+
+def write(sf):
+    data = "Confidentiality,Integrity,Authenticity,Controlled Access,Accountability,Availability"
+    sf.write("{}\n".format(data))
+    sf.flush()
+
+read(sf)
+read(sf)
+read(sf)
+read(sf)
+read(sf)
+read(sf)
+write(sf)
+read(sf)
+read(sf)
+read(sf)
